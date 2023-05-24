5d61785d5c9400f0d085700c090d2d4172239537
khushbu <khushbusoni475@gmail.com>
2023-05-25 02:35:26 +0530

    [khushbu] modify Perfact Number

---
+++ Day6LogicalPrograms/PerfactNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    internal class PerfactNumber
+    {
+        public static void Factors_Addition(int n)
+        {
+            int sum = 0;
+            for(int i = 1; i < n; i++)
+            {
+              if(n % i == 0)
+                {
+                  sum += i;
+                }
+            }
+            
+            if (sum == n)
+            {
+                Console.WriteLine("{0} is perfact number", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a perfect number", n);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
--- Day6LogicalPrograms/Program.cs
+++ Day6LogicalPrograms/Program.cs
@@ -11,10 +11,34 @@ namespace Day6LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the logical programs");
-            Console.WriteLine("please enter any number of which you want fibonoccie series");
-            int n = Convert.ToInt32(Console.ReadLine());
-            FibonoccieSeries.FibonoccieNumber(n);
-            Console.ReadLine();
+            Console.WriteLine("Choose any one program");
+            Console.WriteLine("1.Fibonoccie Series\n2.Perfact Number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("please enter any number of which you want fibonoccie series");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        FibonoccieSeries.FibonoccieNumber(number);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("please enter eny number");
+                        int n = Convert.ToInt32(Console.ReadLine());
+                        PerfactNumber.Factors_Addition(n);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("please choose existing program");
+                        break;
+                    }
+                    Console.ReadLine();
+            }
+            
+            
         }
     }
 }
